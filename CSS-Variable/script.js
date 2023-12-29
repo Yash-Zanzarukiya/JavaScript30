56604313561f0c2816a143957f87b451972c3438
Yash-Zanzarukiya <zanzarukiyayash2711@gmail.com>
2023-12-30 00:45:27 +0530

    Project Finished

--- CSS-Variable/script.js
+++ CSS-Variable/script.js
@@ -1,25 +1,50 @@
 let picture = document.getElementById(('picture'));
 
-let blur = document.querySelector('#blur');
-let width = document.querySelector('#width');
-let color = document.querySelector('#color');
-
 let inputs = document.querySelectorAll('input');
 
-inputs.forEach(input => input.addEventListener('change',updateValue))
+inputs.forEach(input => input.addEventListener('change',updateValue));
+inputs.forEach(input => input.addEventListener("mousemove",updateValue));
 
 function updateValue(){
-    
+    let suffix, name;
+
+    if(this.name == 'color'){
+        suffix='';
+        name = 'border-color';
+    }
+    else if (this.name == 'blur') {
+        suffix='px';
+        name = 'blur-value';
+    }
+    else if (this.name == 'width') {
+        suffix='px';
+        name = 'border-width';
+    }
+    document.documentElement.style.setProperty(`--${name}`,this.value + suffix);
 }
 
-function setColor(color){
-    picture.style.borderColor = color;
-}
 
-function setBorderWidth(width){
-    picture.style.borderWidth = `${width}px`;
-}
+// function updateValue(){
 
-function setBlurValue(blur_value){
-    picture.style.filter = `blur(${blur_value}px)`;
-}
+//     let value = this.value;
+
+//     if(this.name == 'color')
+//         setColor(value);
+//     else if (this.name == 'blur')
+//         setBlurValue(value);
+//     else if (this.name == 'width')
+//         setBorderWidth(value);
+// }
+
+// function setColor(color){
+//     picture.style.borderColor = color;
+//     boldtag.style.color = color;
+// }
+
+// function setBorderWidth(width){
+//     picture.style.borderWidth = `${width}px`;
+// }
+
+// function setBlurValue(blur_value){
+//     picture.style.filter = `blur(${blur_value}px)`;
+// }
